e1cee11e39f30e09a3fa5e376dee8677928b952d
pshvaa <sabira.pshva@gmail.com>
2022-11-02 00:41:18 +0400

    created

---
+++ t_array.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int[]  arr =new int[6] { 2,1,7,8,9,10};
+            int max = 0;
+            int min = 0;
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i]>max)
+                {
+                    max = arr[i];
+                }
+                else 
+                {
+                    min = arr[i];
+                }
+
+            }
+            Console.WriteLine(max-min);
+
+        }
+
+    }
+}
